3ad21a8fc4acd64f14279fc88ebc03f31a41767c
Pavithra <pavithrakasukurthi00@gmail.com>
2026-05-27 19:34:10 +0530

    terraform

--- data.tf
+++ data.tf
@@ -31,9 +31,9 @@ data "aws_ssm_parameter" "vpc_id" {
 }
 
 data "aws_ssm_parameter" "frontend_alb_listener_arn" {
-    name = "/${var.project}/${var.environment}/frontend_alb_listener_arn
+    name = "/${var.project}/${var.environment}/frontend_alb_listener_arn"
 }
 
 data "aws_ssm_parameter" "backend_alb_listener_arn" {
-    name = "/${var.project}/${var.environment}/backend_alb_listener_arn
+    name = "/${var.project}/${var.environment}/backend_alb_listener_arn"
 }
